7b21f5bb23d39239625298d388ca6ee463eeada5
Heyajerry <96361414+JerryIs-strong@users.noreply.github.com>
2022-08-14 13:32:43 +0800

    Update mJudger.js

--- JS/mJudger.js
+++ JS/mJudger.js
@@ -4,7 +4,7 @@ var devices = ["iPhone","Android","Windows Phone"]
 var ua = window.navigator.userAgent
 for (var i = 0; i < devices.length; i++) {
 if (ua.indexOf(devices[i]) != -1) {
-   window.location.href = './not-support.html'
+   window.location.href = 'https://jerryis-strong.github.io/jerrypro.github.io/not-support.html'
   }
 }
 }()
@@ -14,5 +14,16 @@ window.addEventListener('contextmenu', function (e) {
   // do something here... 
   e.preventDefault(); 
 }, false);
+
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    document.getElementById("navbarH").style.top = "0";
+  } else {
+    document.getElementById("navbarH").style.top = "-50px";
+  }
+}
 
 
