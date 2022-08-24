28af4fa627809852c7b7cccc2cf4c2f788f34f1c
Heyajerry <96361414+JerryIs-strong@users.noreply.github.com>
2022-08-24 12:58:51 +0800

    Add files via upload

---
+++ JS/typing.js
@@ -0,0 +1,8 @@
+!function(){
+    var typed = new Typed(".TypingText", {
+      strings: ["夢想：成爲小米工程師", "日常：打code", "老婆：和泉紗霧", "學校：PLK83", "電腦：ASUS"],
+      typeSpeed: 150,
+      backSpeed: 150,
+      loop: true
+    })
+  }()
