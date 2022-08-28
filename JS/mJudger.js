902905ca6c786170d4fa39d8d5c4b58a72ddbb76
Heyajerry <96361414+JerryIs-strong@users.noreply.github.com>
2022-08-28 23:31:55 +0800

    Update mJudger.js

--- JS/mJudger.js
+++ JS/mJudger.js
@@ -15,15 +15,3 @@ window.addEventListener('contextmenu', function (e) {
   e.preventDefault(); 
 }, false);
 
-// When the user scrolls down 20px from the top of the document, slide down the navbar
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-    document.getElementById("navbarH").style.top = "0";
-  } else {
-    document.getElementById("navbarH").style.top = "-50px";
-  }
-}
-
-
