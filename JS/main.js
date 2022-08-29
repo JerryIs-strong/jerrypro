a42162baf49b26d62f5648d5209e98cc3facbd79
Heyajerry <96361414+JerryIs-strong@users.noreply.github.com>
2022-08-29 20:07:11 +0800

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+  function openNav() {
+    document.getElementById("Sidebar").style.width = "250px";
+    document.getElementById("topBG").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("Sidebar").style.width = "0";
+    document.getElementById("topBG").style.marginLeft= "0";
+  }
+
+  function Snackbar() {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  }
