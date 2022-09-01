ec9ed2702aef264d30ef568a65759662e8635df4
Heyajerry <96361414+JerryIs-strong@users.noreply.github.com>
2022-09-01 19:48:47 +0800

    Update mJudger.js

--- JS/mJudger.js
+++ JS/mJudger.js
@@ -1,17 +1,20 @@
 //判斷設備是否為移動終端
 !function () {
-var devices = ["iPhone","Android","Windows Phone"]
-var ua = window.navigator.userAgent
-for (var i = 0; i < devices.length; i++) {
-if (ua.indexOf(devices[i]) != -1) {
-   window.location.href = 'https://jerryis-strong.github.io/jerrypro.github.io/not-support.html'
-  }
-}
-}()
-
-//禁用鼠標右鍵
-window.addEventListener('contextmenu', function (e) { 
-  // do something here... 
-  e.preventDefault(); 
-}, false);
+    var devices = ["iPhone","Android","Windows Phone"]
+    var ua = window.navigator.userAgent
+    for (var i = 0; i < devices.length; i++) {
+    if (ua.indexOf(devices[i]) != -1) {
+       window.location.href = 'https://jerryis-strong.github.io/jerrypro.github.io/not-support.html'
+      }
+    }
+    }()
+    
+    //禁用鼠標右鍵
+    window.addEventListener('contextmenu', function (e) { 
+      var x = document.getElementById("snackbarerror");
+      x.className = "show";
+      setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+      e.preventDefault(); 
+    }, false);
+  
 
