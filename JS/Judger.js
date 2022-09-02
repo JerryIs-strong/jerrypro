a506db3603ae0a4a0660fdb31ca59ad6c59cfdc8
Heyajerry <96361414+JerryIs-strong@users.noreply.github.com>
2022-09-02 19:52:30 +0800

    Update Judger.js

--- JS/Judger.js
+++ JS/Judger.js
@@ -1,6 +1,6 @@
 var is_mobi = navigator.userAgent.toLowerCase().match(/(ipod|ipad|iphone|android|coolpad|mmp|smartphone|midp|wap|xoom|symbian|j2me|blackberry|wince)/i) != null;
 if (is_mobi) {
-    window.location.href = "./index(2nd).html";
+    window.location.href = "./not-support.html";
 }else{
-    window.location.href = "./jerrypro.html";
+    window.location.href = "./index.html";
 }
