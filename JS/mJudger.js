db1e975388199a93eaee6c7b62f8a421a776fdf8
Heyajerry <96361414+JerryIs-strong@users.noreply.github.com>
2022-09-16 20:41:33 +0800

    Update mJudger.js

--- JS/mJudger.js
+++ JS/mJudger.js
@@ -1,14 +1,3 @@
-//判斷設備是否為移動終端
-!function () {
-    var devices = ["iPad"]
-    var ua = window.navigator.userAgent
-    for (var i = 0; i < devices.length; i++) {
-    if (ua.indexOf(devices[i]) != -1) {
-       window.location.href = './ipad.html'
-      }
-    }
-    }()
-    
     //禁用鼠標右鍵
     window.addEventListener('contextmenu', function (e) { 
       var x = document.getElementById("snackbarerror");
