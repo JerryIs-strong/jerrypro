bde46381afec95d7ffe539741862ce51ad518e12
Heyajerry <96361414+JerryIs-strong@users.noreply.github.com>
2022-09-16 20:13:51 +0800

    Add files via upload

---
+++ JS/Judgeripad.js
@@ -0,0 +1,40 @@
+/*var not_mobi = navigator.userAgent.toLowerCase().match(/(Windows|Mac)/i) != null;
+if (not_mobi) {
+    window.location.href = "./jerrypro.html";
+}*/
+
+/*if (/(iPhone|iPad|iPod|iOS|Android)/i.test(navigator.userAgent)) {
+    //alert(navigator.userAgent);  
+    window.location.href ="#";
+}else {
+    window.location.href ="./jerrypro.html";
+}*/
+
+var browser={
+    versions:function(){ 
+           var u = navigator.userAgent, app = navigator.appVersion; 
+           return {
+                trident: u.indexOf('Trident') > -1, 
+                presto: u.indexOf('Presto') > -1, 
+                webKit: u.indexOf('AppleWebKit') > -1, 
+                gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, 
+                mobile: !!u.match(/AppleWebKit.*Mobile.*/), 
+                ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), 
+                android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, 
+                iPhone: u.indexOf('iPhone') > -1 , 
+                iPad: u.indexOf('iPad') > -1, 
+                webApp: u.indexOf('Safari') == -1, 
+                weixin: u.indexOf('MicroMessenger') > -1, 
+                qq: u.match(/\sQQ/i) == " qq" 
+            };
+         }(),
+         language:(navigator.browserLanguage || navigator.language).toLowerCase()
+} 
+
+if(browser.versions.iPad){  	
+  window.location.href="#ture";
+}else{
+  window.location.href="./jerrypro.html";
+}
+
+/*code from : https://bit.ly/3eB0LQ6*/
