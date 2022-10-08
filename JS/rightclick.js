59f5c11dc66d328d2d7bad928b525a0a13ba7976
Heyajerry <96361414+JerryIs-strong@users.noreply.github.com>
2022-10-08 14:29:12 +0800

    Delete rightclick.js

--- JS/rightclick.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('contextmenu', function (e) { 
-    var x = document.getElementById("snackbar");
-    x.className = "show";
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-    e.preventDefault(); 
-  }, false);
