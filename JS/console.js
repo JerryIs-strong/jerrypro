55cebc1e974919cdf9f0880ff8e151bf2a3910d5
Heyajerry <96361414+JerryIs-strong@users.noreply.github.com>
2022-10-11 19:39:25 +0800

    Update console.js

--- JS/console.js
+++ JS/console.js
@@ -10,7 +10,7 @@ const ascii =
 
 console.log(ascii)          
 console.log(
-    'Powered by %cJERRY_PRO%cV1.2.1',
+    'Powered by %cJERRY_PRO%cV1.3.1',
     'display: inline—block;background-color:rgba(66,66,66,0.8);color:#fff;margin—bottom:6px;padding:4px;border-radius:4px 0 0 4px',
     'display: inline-block;background-color:rgba(0,180,0,1);color:#fff;padding:4px;border-radius:0 4px 4px 0', 
 )                                                             
