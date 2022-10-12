2332acf85d3e352556a0aeb5fde0a534141bec04
Heyajerry <96361414+JerryIs-strong@users.noreply.github.com>
2022-10-12 15:22:41 +0800

    Update console.js

--- JS/console.js
+++ JS/console.js
@@ -1,16 +1,17 @@
 const ascii =
-"*       ##                                                   \n"+
+"*        ##                                                   \n"+
 "*                                                            \n"+
-"*      ###    ####    ######   ######   ##  ##               \n"+
-"*       ##   ##  ##    ##  ##   ##  ##  ##  ##               \n"+
-"*       ##   ######    ##       ##      ##  ##     ==        \n"+
-"*   ##  ##   ##        ##       ##       #####               \n"+
-"*   ##  ##    #####   ####     ####         ##               \n"+
+"*       ###    ####    ######   ######   ##  ##               \n"+
+"*        ##   ##   ##  ##  ##   ##  ##   ##  ##               \n"+
+"*        ##   ######   ##       ##       ##  ##     ==        \n"+
+"*   ##   ##   ##       ##       ##       #####               \n"+
+"*   ##   ##    #####   ####     ####        ##               \n"+
 "*    ####                               #####                \n";
 
 console.log(ascii)          
 console.log(
-    'Powered by %cJERRY_PRO%cV1.3.1',
+    'Powered by %cJERRY_PRO%cV1.3.1\n', 
     'display: inline—block;background-color:rgba(66,66,66,0.8);color:#fff;margin—bottom:6px;padding:4px;border-radius:4px 0 0 4px',
     'display: inline-block;background-color:rgba(0,180,0,1);color:#fff;padding:4px;border-radius:0 4px 4px 0', 
-)                                                             
+    'Latest on 12st October\n',
+) 
