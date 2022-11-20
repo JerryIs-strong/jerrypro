0bd33684ed0bddde383e82ce2b87d10642fef3cf
Heyajerry <96361414+JerryIs-strong@users.noreply.github.com>
2022-11-20 22:46:48 +0800

    Update console.js

--- JS/console.js
+++ JS/console.js
@@ -12,5 +12,10 @@ console.log(
     'Powered by %cJERRY_PRO%cV1.3.1\n', 
     'display: inline—block;background-color:rgba(66,66,66,0.8);color:#fff;margin—bottom:6px;padding:4px;border-radius:4px 0 0 4px',
     'display: inline-block;background-color:rgba(0,180,0,1);color:#fff;padding:4px;border-radius:0 4px 4px 0', 
-    'Latest on 15st October\n',
+    'Latest on 12st October\n',
 ) 
+
+const alert = "//警告！⚠️";
+const alertA = "使用這個主控台可能會讓攻擊者有機會利用名為 Self-XSS 的攻擊方式冒用你的身分，然後竊取你的資訊。請勿輸入或貼上來路不明的程式碼。//"
+console.log('%c'+ alert,'color: red;background-color: yellow;font-size: 25px;font-weight: 500;')
+console.log('%c'+ alertA,'font-size: 15px;')
