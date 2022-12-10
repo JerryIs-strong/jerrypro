815b8513f681a651ed44031d86c9ddd62256d23b
Heyajerry <96361414+JerryIs-strong@users.noreply.github.com>
2022-12-10 17:42:28 +0800

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -92,24 +92,25 @@ function Themebgloder() {
   var x = document.getElementById("topBG");
   var randomNumber = Math.floor(Math.random() * 5);
   document.body.id = "Done";
+  x.style.backgroundImage = "url('http://api.btstu.cn/sjbz/?lx=dongman')"
 
-  if (randomNumber == "1") {
-    x.style.backgroundImage =
-      "url('https://jerrypro.xyz/Resources/headerBg/small.jpg')";
-    console.log("Theme: 1");
-  } else if (randomNumber == "2") {
-    x.style.backgroundImage =
-      "url('https://jerrypro.xyz/Resources/headerBg/small2.jpg')";
-    console.log("Theme: 2");
-  } else if (randomNumber == "3") {
-    x.style.backgroundImage =
-      "url('https://jerrypro.xyz/Resources/headerBg/small3.jpg')";
-    console.log("Theme: 3");
-  } else {
-    x.style.backgroundImage =
-      "url('https://jerrypro.xyz/Resources/headerBg/small4.jpg')";
-    console.log("Theme: 4");
-  }
+  // if (randomNumber == "1") {
+  //   x.style.backgroundImage =
+  //     "url('https://jerrypro.xyz/Resources/headerBg/small.jpg')";
+  //   console.log("Theme: 1");
+  // } else if (randomNumber == "2") {
+  //   x.style.backgroundImage =
+  //     "url('https://jerrypro.xyz/Resources/headerBg/small2.jpg')";
+  //   console.log("Theme: 2");
+  // } else if (randomNumber == "3") {
+  //   x.style.backgroundImage =
+  //     "url('https://jerrypro.xyz/Resources/headerBg/small3.jpg')";
+  //   console.log("Theme: 3");
+  // } else {
+  //   x.style.backgroundImage =
+  //     "url('https://jerrypro.xyz/Resources/headerBg/small4.jpg')";
+  //   console.log("Theme: 4");
+  // }
 }
 
 function bplayer() {
