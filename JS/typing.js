7035b4b1678a73c96bc28116d55163f4218fb000
Heyajerry <96361414+JerryIs-strong@users.noreply.github.com>
2023-01-02 16:04:25 +0800

    Update typing.js

--- JS/typing.js
+++ JS/typing.js
@@ -1,6 +1,6 @@
 !function(){
     var typed = new Typed(".TypingText", {
-      strings: ["夢想：成爲小米工程師", "日常：打code", "老婆：和泉紗霧", "學校：PLK83", "電腦：ASUS"],
+      strings: ["夢想：成爲小米工程師", "日常：打code", "老婆：Koala", "學校：PLK83", "電腦：ASUS"],
       typeSpeed: 150,
       backSpeed: 150,
       loop: true
